ce8b8290c4124d4ddb97be3f28528b51de8f4147
Rebecca Cran <rebecca@quicinc.com>
2022-12-06 22:30:39 +0800

    MdeModulePkg: Put USB DEBUGs that occur for bulk timeouts under VERBOSE
    With the pending commit of UsbNetworkPkg, it will become common for
    UsbBulkTransfer calls to timeout, given that the drivers are called from
    MnpSystemPoll every MNP_SYS_POLL_INTERVAL milliseconds: the drivers
    check for network packets by calling UsbBulkTransfer with a timeout of
    1ms.
    
    Avoid console spam by moving DEBUG messages that occur each time a bulk
    transfer request times out from DEBUG_ERROR to DEBUG_VERBOSE, for both
    EHCI and XHCI drivers.
    
    Signed-off-by: Rebecca Cran <rebecca@quicinc.com>
    Reviewed-by: Hao A Wu <hao.a.wu@intel.com>

--- MdeModulePkg/Bus/Pci/XhciDxe/Xhci.c
+++ MdeModulePkg/Bus/Pci/XhciDxe/Xhci.c
@@ -1235,6 +1235,7 @@ XhcBulkTransfer (
   UINT8              SlotId;
   EFI_STATUS         Status;
   EFI_TPL            OldTpl;
+  UINTN              DebugErrorLevel;
 
   //
   // Validate the parameters
@@ -1297,7 +1298,13 @@ XhcBulkTransfer (
 
 ON_EXIT:
   if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "XhcBulkTransfer: error - %r, transfer - %x\n", Status, *TransferResult));
+    if (Status == EFI_TIMEOUT) {
+      DebugErrorLevel = DEBUG_VERBOSE;
+    } else {
+      DebugErrorLevel = DEBUG_ERROR;
+    }
+
+    DEBUG ((DebugErrorLevel, "XhcBulkTransfer: error - %r, transfer - %x\n", Status, *TransferResult));
   }
   gBS->RestoreTPL (OldTpl);
 
